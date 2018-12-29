4d1c48eaa0dab956ba85cfb9023a29dfb9bc41a6
Brian Thomas <Brianthomas571@gmail.com>
2018-12-29 12:07:23 -0600

    added player form component and stats component

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,38 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Player from "./Player";
+import AddPlayerForm from "./AddPlayerForm";
 
 class App extends Component {
   state = {
     players: [
       {
         name: "Brian",
+        score: 0,
         id: 1
       },
       {
         name: "Tom Brady",
+        score: 0,
         id: 2
       },
       {
         name: "Gronk",
+        score: 0,
         id: 3
       },
       {
         name: "LeBron James",
+        score: 0,
         id: 4
       }
     ]
+  };
+
+  handleScoreChange = (index, difference) => {
+    this.setState(prevState => ({
+      score: (prevState.players[index].score += difference)
+    }));
   };
 
   handleRemovePlayer = id => {
@@ -35,17 +46,22 @@ class App extends Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+        <Header title="Scoreboard" players={this.state.players} />
 
         {/* Players list */}
-        {this.state.players.map(player => (
+        {this.state.players.map((player, index) => (
           <Player
             name={player.name}
+            index={index}
+            score={player.score}
             id={player.id}
             key={player.id.toString()}
+            changeScore={this.handleScoreChange}
             removePlayer={this.handleRemovePlayer}
           />
         ))}
+
+        <AddPlayerForm />
       </div>
     );
   }
